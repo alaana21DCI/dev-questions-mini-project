786948339f78480b87ea6a9b4b78899aa258a073
alaa nasser <alaanasser749@gmail.com>
2022-10-10 19:23:16 +0200

    created login Form UI

--- frontend/src/routes/login/index.js
+++ frontend/src/routes/login/index.js
@@ -1,13 +1,103 @@
 import * as React from "react";
 import "./index.scss";
-import Layout from "../../Layout";
 
-const Login = () => {
+import Input from "../../UI/Input";
+import Button from "../../UI/Button";
+
+export default function Login() {
+  const [showRegister, setShowRegister] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  // Bei der Registrirung soll der User optinal ein Profilbild hochladen können.
+  const [profilePic, setProfilePic] = React.useState();
+
+  if (showRegister) {
+    // Registierung-Formular
+    return (
+      <div className="Login">
+        <div className="wrapper">
+          <form onSubmit="" className="form-box">
+            <div className="head">
+              <h1>Register Formular</h1>
+              <div
+                className="toggle-register"
+                onClick={() => setShowRegister(false)}
+              >
+                Ich habe bereits einen Account!
+              </div>
+            </div>
+            <Input
+              id="email"
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              id="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Input
+              id="name"
+              label="Name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              id="profilbild"
+              label="Profilbild"
+              type="file"
+              accept="image/*"
+              onChange={(e) => setProfilePic(e.target.files[0])}
+            />
+            <div className="actions">
+              <Button type="submit" className="btn">
+                Abschicken
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <Layout>
-      <h1>Login</h1>
-    </Layout>
+    <div className="Login">
+      <div className="wrapper">
+        <form onSubmit="" className="form-box">
+          <div className="head">
+            <h1>Login Formular </h1>
+            <div
+              className="toggle-register"
+              onClick={() => setShowRegister(true)}
+            >
+              Ich habe noch keinen Account!
+            </div>
+          </div>
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Fetching
+          </button>
+        </form>
+      </div>
+    </div>
   );
-};
-
-export default Login;
+}
