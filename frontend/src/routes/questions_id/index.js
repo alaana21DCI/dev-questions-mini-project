c0ade1243ff7dbe1c1f343feb7d7a4cccab1b769
alaa nasser <alaanasser749@gmail.com>
2022-10-11 00:17:23 +0200

    created question view

--- frontend/src/routes/questions_id/index.js
+++ frontend/src/routes/questions_id/index.js
@@ -1,15 +1,50 @@
 import * as React from "react";
 import "./index.scss";
-import { useParams } from "react-router-dom";
-import Layout from "../../Layout";
 
-const Question = () => {
+import Layout from "../../Layout";
+import { useParams } from "react-router-dom";
+
+export default function Question() {
   const params = useParams();
+  const [question, setQuestion] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/questions/" + params.id, {
+      method: "GET",
+      credentials: "include",
+    }).then(async (response) => {
+      const result = await response.json();
+
+      if (response.status === 200) {
+        setQuestion(result);
+      }
+    });
+  }, [params.id]);
+
+  if (!question) {
+    return (
+      <Layout>
+        <h2>Loaing...</h2>
+      </Layout>
+    );
+  }
+
   return (
+    // 4. Bei den Antworten soll das Profilbild angezeigt werden falls es vorhanen ist.
+    // dazu brauchen den profileImage field auf user im Backend user sowie auf antwort  zu papulaten im questionsController
+
     <Layout>
-      <h1>Question [{params.id}] </h1>
+      <div className="QuestionId">
+        <div className="wrapper">
+          <section className="question-box">
+            <div className="head">
+              <h1 className="title">{question.title}</h1>
+              <span className="name">{question.user.name}</span>
+            </div>
+            <div className="description">{question.description}</div>
+          </section>
+        </div>
+      </div>
     </Layout>
   );
-};
-
-export default Question;
+}
